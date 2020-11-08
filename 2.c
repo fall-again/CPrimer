6addb30832b75ef7d2da775ecd3733ca971b49f3
fall-again <15022673515@163.com>
2020-11-08 15:18:56 +0800

    master

---
+++ 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    scanf("%d%d", &num1, &num2);
+    int sum = num2 + num1;
+    printf("%d\n", sum);
+    system("pause");
+    return 0;
+}
