8c10412cc187b0db28cc981408b0f2bf27aad923
HZQ <15022673515@163.com>
2021-03-30 07:51:28 +0800

    master

--- 01Basic/03.c
+++ 01Basic/03.c
@@ -2,7 +2,7 @@
  * @Description: basic
  * @Author: HZQ
  * @Date: 2021-03-28 09:01:35
- * @LastEditTime: 2021-03-29 11:35:22
+ * @LastEditTime: 2021-03-29 18:54:59
  */
 
 #include <stdio.h>
@@ -16,6 +16,12 @@
 
 //     return;
 // }
+
+struct Book
+{
+    char name[20]; // 书名
+    int price;     // 价钱
+};
 
 int main(void)
 {
@@ -35,11 +41,30 @@ int main(void)
     //     i++;
     // }
 
-    int num = 10;
-    int *p;
-    p = &num;
-    printf("%p\n", &num);
-    printf("%p\n", p);
+    // int num = 10;
+    // int *p;
+    // double d = 3.14;
+    // double *p1 = &d;
+    // p = &num;
+
+    // printf("%d\n", sizeof(char *));
+    // printf("%d\n", sizeof(short *));
+    // printf("%d\n", sizeof(int *));
+    // printf("%d\n", sizeof(double *));
+    // printf("%d\n", sizeof(p1));
+
+    // printf("%p\n", &num);
+    // printf("%p\n", p);
+
+    struct Book b1 = {"一个", 60};
+    printf("name = %s \n", b1.name);
+    printf("price = %d \n", b1.price);
+
+    struct Book *pb = &b1;
+    printf("name = %s \n", (*pb).name);
+    printf("price = %d \n", (*pb).price);
+    printf("name = %s \n", pb->name);
+    printf("price = %d \n", pb->price);
 
     getchar();
     return 0;
---
+++ 01Basic/04.c
@@ -0,0 +1,40 @@
+/*
+ * @Description:1. 判断一个数是否为奇数
+ *              2. 输出1-100之间的奇数
+ * @Author: HZQ
+ * @Date: 2021-03-29 20:38:54
+ * @LastEditTime: 2021-03-29 21:29:44
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // printf("请输入一个数：\n");
+    // int num1;
+    // scanf("%d", &num1);
+    // if (num1 % 2 == 0)
+    // {
+    //     printf("该数字为偶数\n");
+    // }
+    // else
+    // {
+    //     printf("该数字为奇数\n");
+    // }
+
+    int num2;
+    for (num2 = 1; num2 <= 100; num2++)
+    {
+        if (num2 % 2 == 0)
+        {
+            //printf("该数字为偶数\n");
+        }
+        else
+        {
+            printf("%d\n", num2);
+        }
+    }
+
+    return 0;
+}
